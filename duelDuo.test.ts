0fc045aacffd5287938800f2244656ceffeb5892
Rafael Daminelli <rdaminelli@hotmail.com>
2022-12-18 02:57:11 -0600

    3rd commit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -21,6 +21,19 @@ test('Title shows up when page loads', async () => {
 
 
 test('Check that clicking the Draw button displays the div with id = “choices”', async () => {
-    const drawBtn = await driver.findElement(By.id('draw')).click()
-    
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
+    const bots = await driver.findElement(By.id('choices'))
+    const choices = await bots.isDisplayed()
+    expect(choices).toBe(true)  
+})
+
+test('Check that clicking an “Add to Duo” button displays the div with id = “player-duo”', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
+    await driver.findElement(By.className('bot-btn')).click()
+    await driver.sleep(2000) 
+    const playerDuo = await driver.findElement(By.id('player-duo'))
+    const chosen = await playerDuo.isDisplayed()
+    expect(chosen).toBe(true)     
 })
